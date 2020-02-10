62dbd6ef56b4d177b92b2f1b299f0d210f80015a
尾原颯 <ohara.soh@unipro.co.jp>
2020-02-10 21:16:09 +0900

    Added download script

---
+++ download.sh
@@ -0,0 +1,6 @@
+mkdir data
+wget https://www.aozora.gr.jp/index_pages/list_person_all_extended_utf8.zip -P data
+unzip data/list_person_all_extended_utf8.zip
+mv list_person_all_extended_utf8.csv data/list_person_all_extended_utf8.csv
+python 00_download.py 
+python 01_clean_text.py 
